ff1ad76831e2618960cd51643f737cddf141d17c
William <will2144@icloud.com>
2023-04-12 11:30:46 -0400

    Add a new Shopping list Class

---
+++ src/main/java/com/learntocode/ShoppingList.java
@@ -0,0 +1,21 @@
+package com.learntocode;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println ("Milk\nApples");
+                System.out.println ("Apples");
+                System.out.println ("Cucumber");
+                System.out.println ("Gummyworms");
+                System.out.println ("Celery");
+                System.out.println ("Cereal");
+                System.out.println ("Potatoes");
+                System.out.println ("Lettuce");
+                System.out.println ("Water");
+                System.out.println ("mango");
+
+    }
+}
+
+    public static void main(String[] args) {
+
+    }
